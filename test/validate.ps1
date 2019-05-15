2026eec882eb35bffeac971e6195fef5fa75e763
Bernard <bernard@ducourier.com>
2019-05-15 14:55:00 -0400

    Update validation

--- test/validate.ps1
+++ test/validate.ps1
@@ -2,7 +2,8 @@ Param(
     [Parameter(Mandatory = $false)][string]$templateLibraryName = "asg",
     [string]$templateName = "azuredeploy.json",
     [string]$Location = "canadacentral",
-    [string]$subscription = "2de839a0-37f9-4163-a32a-e1bdb8d6eb7e"
+    [string]$subscription = "2de839a0-37f9-4163-a32a-e1bdb8d6eb7e",
+    [bool]$devopsCICD = $false
 )
 
 #******************************************************************************
@@ -19,18 +20,25 @@ function getValidationURL {
     return $validateURL
 }
 
-$currentBranch = git rev-parse --abbrev-ref HEAD
+$currentBranch = dev
+$validationURL = "https://raw.githubusercontent.com/canada-ca-azure-templates/$templateLibraryName/dev/template/masterdeploy.json"
 
-if ($currentBranch -eq 'master') {
-    $confirmation = Read-Host "You are working off the master branch... are you sure you want to validate the template from here? Switch to the dev branch is recommended. Continue? (y/n)"
-    if ($confirmation -ne 'y') {
-        exit
+if (-not $devopsCICD) {
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+
+    if ($currentBranch -eq 'master') {
+        $confirmation = Read-Host "You are working off the master branch... are you sure you want to validate the template from here? Switch to the dev branch is recommended. Continue? (y/n)"
+        if ($confirmation -ne 'y') {
+            exit
+        }
     }
-}
 
-# Make sure we update code to git
-# git branch dev ; git checkout dev ; git pull origin dev
-git add . ; git commit -m "Update validation" ; git push origin $currentBranch
+    $validationURL = getValidationURL
+
+    # Make sure we update code to git
+    # git branch dev ; git checkout dev ; git pull origin dev
+    git add . ; git commit -m "Update validation" ; git push origin $currentBranch
+}
 
 Select-AzureRmSubscription -Subscription $subscription
 
@@ -54,7 +62,6 @@ if ($provisionningState -eq "Failed") {
 # Validating server template
 Write-Host "Starting $templateLibraryName validation deployment...";
 
-$validationURL = getValidationURL
 New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Name "validate-$templateLibraryName-template" -TemplateUri $validationURL -TemplateParameterFile (Resolve-Path "$PSScriptRoot\parameters\validate.parameters.json") -Verbose
 
 $provisionningState = (Get-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Name "validate-$templateLibraryName-template").ProvisioningState
