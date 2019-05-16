14d0135f17d566ac8031da6aa7daf8757a559ad0
Bernard <bernard@ducourier.com>
2019-05-16 07:16:12 -0400

    Update validation

--- test/validate.ps1
+++ test/validate.ps1
@@ -1,8 +1,7 @@
 Param(
-    [Parameter(Mandatory = $false)][string]$templateLibraryName = "asg",
-    [string]$templateName = "azuredeploy.json",
+    [Parameter(Mandatory = $false)][string]$templateLibraryName = (Split-Path (Resolve-Path "$PSScriptRoot\..") -Leaf),
     [string]$Location = "canadacentral",
-    [string]$subscription = "2de839a0-37f9-4163-a32a-e1bdb8d6eb7e",
+    [string]$subscription = "",
     [switch]$devopsCICD = $false
 )
 
@@ -40,7 +39,9 @@ if (-not $devopsCICD) {
     git add . ; git commit -m "Update validation" ; git push origin $currentBranch
 }
 
-Select-AzureRmSubscription -Subscription $subscription
+if ($subscription -ne "") {
+    Select-AzureRmSubscription -Subscription $subscription
+}
 
 # Cleanup validation resource content in case it did not properly completed and left over components are still lingeringcd
 Write-Host "Cleanup old $templateLibraryName validation resources if needed...";
